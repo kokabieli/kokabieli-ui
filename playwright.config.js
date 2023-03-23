9a3b22de80443eeb90a7f107b3baf8b859cecad4
Florian Schrag <florian@schrag.org>
2023-03-23 20:07:35 +0100

    add more timeouts

--- playwright.config.js
+++ playwright.config.js
@@ -3,11 +3,14 @@ const config = {
 	webServer: {
 		command: 'npm run build && npm run preview',
 		port: 4173,
-		timeout: 120000
+		timeout: 120*1000
 	},
 	testDir: 'tests',
-	timeout: 120000,
-	globalTimeout: 120000,
+	timeout: 120*1000,
+	globalTimeout: 60*60*1000,
+	expect: {
+		timeout: 10*1000
+	}
 };
 
 export default config;
