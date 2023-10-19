7a9280389f95fa958c209b231e791f2f2d49ac1c
Florian Schrag <florian@schrag.org>
2023-10-19 12:49:22 +0200

    test should also work without running server

--- tests/test.js
+++ tests/test.js
@@ -1,20 +1,31 @@
-import { expect, test } from '@playwright/test';
+import { expect, test } from "@playwright/test";
 
 
-test('index page should show kokabieli', async ({ page }) => {
-	await page.goto('/?file=sample.json');
-	expect(await page.$('svg')).toBeTruthy();
-	await page.screenshot({ path: 'screenshots/show-kokabieli.png', fullPage: true });
-	expect(await page.textContent('h3')).toBe('kokabieli');
+async function sampleConstellation(page) {
+    await page.route("/constellation-data/*", async route => {
+        const file = route.request().url().split("/").pop();
+        await route.fulfill({
+            path: `static/sample-constellation-data/${file}`
+        });
+    });
+}
+
+test("index page should show kokabieli", async ({ page }) => {
+    sampleConstellation(page).then(r => console.log(r));
+    await page.goto("/?file=sample.json");
+    expect(await page.$("svg")).toBeTruthy();
+    await page.screenshot({ path: "screenshots/show-kokabieli.png", fullPage: true });
+    expect(await page.textContent("h3")).toBe("kokabieli");
 });
 
-test('index page should show a graph', async ({ page }) => {
-	await page.goto('/?file=sample.json');
-	expect(await page.$('svg')).toBeTruthy();
-	await page.screenshot({ path: 'screenshots/root-index.png', fullPage: true });
-	expect(await page.$('id=portfolio_flat')).toBeTruthy();
-	await page.screenshot({ path: 'screenshots/root-graphed.png', fullPage: true });
-	await page.click('id=portfolio_flat');
-	expect(await page.textContent('h2')).toBe('portfolio (flat)');
-	await page.screenshot({ path: 'screenshots/root-clicked.png', fullPage: true });
+test("index page should show a graph", async ({ page }) => {
+    sampleConstellation(page).then(r => console.log(r));
+    await page.goto("/?file=sample.json");
+    expect(await page.$("svg")).toBeTruthy();
+    await page.screenshot({ path: "screenshots/root-index.png", fullPage: true });
+    expect(await page.$("id=portfolio_flat")).toBeTruthy();
+    await page.screenshot({ path: "screenshots/root-graphed.png", fullPage: true });
+    await page.click("id=portfolio_flat");
+    expect(await page.textContent("h2")).toBe("portfolio (flat)");
+    await page.screenshot({ path: "screenshots/root-clicked.png", fullPage: true });
 });
